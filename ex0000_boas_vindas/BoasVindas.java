98db9b11bc4d5580f3c3dc9ee92a4c931c82a19a
Danielle <danielle8farias@gmail.com>
2021-08-11 21:04:34 -0300

    boas vindas

---
+++ ex0000_boas_vindas/BoasVindas.java
@@ -0,0 +1,7 @@
+public class BoasVindas {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Danielle";
+        System.out.println("Bem-vinda, "+ nome + "!");
+    }
+}
